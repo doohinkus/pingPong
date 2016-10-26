8ab60db77b22645d89e0ee856ab4e3c68366b582
Amy Hwang and Rafael Perez <amy+rafael@>
2016-10-26 14:04:57 -0700

    include words that start with sq in the translation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,31 @@
 var pigLatin = function (words){
+  // words.toLowerCase();
   var letters = words.split("");
-  var vowels = ["a", "e", "i", "o", "u"];
-  var firstletter = letters[0];
+  var vowels = ["a", "e", "i", "o", "u", "y"];
+  var firstLetter = letters[0];
+  var secondLetter = letters[1];
   var consonants = "";
   console.log(letters)
 
-  if (words.search(/[^a-zA-Z\s]+/) != -1) {
+  if ((words.search(/[^a-zA-Z\s]+/) != -1)) {
     return "Enter only alphebat letters.";
-  }else if (vowels.indexOf(firstletter) != -1){
+  }else if (vowels.indexOf(firstLetter) != -1){
+    if(firstLetter === "y"){
+      letters.splice(0, 1);
+      letters.push("y");
+    }
      letters.push("ay");
      return letters.join("");
-  } else {
+   } else if (firstLetter === "s" && secondLetter === "q"){
+     letters.splice(0, 2);
+     letters.push("squay");
+     return letters.join("");
+   }else if (firstLetter === "q"){
+     letters.splice(0, 2);
+     letters.push("quay");
+     return letters.join("");
+     console.log("q!!!!", letters);
+   }else {
     //regular consonants
       for (var i=0; i < letters.length; i++){
           if (vowels.indexOf(letters[i]) == -1){
